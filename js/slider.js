6f568c1e048e36288e42042a672749ce81c324d7
Asiia Pakhomova <askapakhomova@gmail.com>
2019-09-27 16:29:32 +0300

    slider

--- js/slider.js
+++ js/slider.js
@@ -3,23 +3,30 @@ const right = document.querySelector('#right');
 let slider = document.querySelector('#slider');
 const computed = getComputedStyle(slider);
 
-
+var currentRight = parseInt(computed.right);
 
 right.addEventListener('click', function(e) {
 e.preventDefault();
 
-let currentRight = parseInt(computed.right);
-let sliderElem = document.querySelector('.burger-slider__item');
-let width = sliderElem.offsetWidth;
-let currentWidth = width * 4;
+// let currentRight = parseInt(computed.right);
+// let sliderElem = document.querySelector('.burger-slider__item');
+// let width = sliderElem.offsetWidth;
+// let currentWidth = width * 4;
 
 
-if(!currentRight) {
-  currentRight = 0;
-}
+// if(!currentRight) {
+//   currentRight = 0;
+// }
 
-if(currentRight < currentWidth){
-  slider.style.right = currentRight + width + "px";
+// if(currentRight < currentWidth){
+//   slider.style.right = currentRight + width + "px";
+
+// }
+
+if(currentRight < 400){
+  slider.style.right = currentRight + 100 + "%";
+  currentRight+=100;
+  console.log(currentRight);
 
 }
 
@@ -27,18 +34,20 @@ if(currentRight < currentWidth){
 
 left.addEventListener('click', function(e) {
   e.preventDefault(); 
-  let currentRight = parseInt(computed.right);
-  let sliderElem = document.querySelector('.burger-slider__item');
-  let width = sliderElem.offsetWidth;
-  let currentWidth = width * 4;
+
+  // let sliderElem = document.querySelector('.burger-slider__item');
+  // let width = sliderElem.offsetWidth;
+  // let currentWidth = width * 4;
 
 
 if(!currentRight) {
   currentRight = 0;
 }
 
-  if(currentRight > 0){
-    slider.style.right = currentRight - width + "px";
+  if(currentRight > 100){
+    currentRight-=100;
+    slider.style.right = currentRight - 100 + "%";
   
+    console.log(currentRight);
   }
 });
